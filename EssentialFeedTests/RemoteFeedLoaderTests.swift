84ce477ba8b92bcaef2cce8ac54e551f83c63ad2
Tarquinio Teles <tarquinioteles@me.com>
2022-10-05 17:20:10 -0300

    added more test assertions for http response codes

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -50,13 +50,16 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
 
-        var capturedError = [RemoteFeedLoader.Error]()
-        sut.load { capturedError.append($0) }
+        let samples = [199,201,404,500]
         
-        let clientError = NSError(domain: "Test", code: 0)
-        client.complete(withStatusCode: 400)
-        
-        XCTAssertEqual(capturedError, [.invalidData])
+        samples.enumerated().forEach { index, code in
+            var capturedError = [RemoteFeedLoader.Error]()
+            sut.load { capturedError.append($0) }
+            
+            client.complete(withStatusCode: code, at:  index)
+            
+            XCTAssertEqual(capturedError, [.invalidData])
+        }
     }
     
     //MARK: -> Helpers
