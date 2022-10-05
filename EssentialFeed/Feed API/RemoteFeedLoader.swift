03024fe57fffb3bfbd55798999e02c1d96f40714
Tarquinio Teles <tarquinioteles@me.com>
2022-10-05 17:29:37 -0300

    replaced optional params in HTTPClient with an enum with success and failure cases

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
+
 public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
@@ -22,16 +31,13 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response  in
-            if response != nil {
-                completion(.invalidData)
-            } else {
-                completion(.connectivity)
+        client.get(from: url) { result  in
+            switch result {
+                case .success:
+                    completion(.invalidData)
+                case .failure:
+                    completion(.connectivity)
             }
         }
     }
-}
-
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
