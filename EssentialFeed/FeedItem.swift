282402c074f974ef9faf495fcb7afd092660b987
Tarquinio Teles <tarquinioteles@me.com>
2022-10-05 08:24:31 -0300

    Created FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Tarquinio Teles on 05/10/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
