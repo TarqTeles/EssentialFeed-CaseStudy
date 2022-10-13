20cfddb34fd3a3e318c462edd062627b6bc1e54d
Tarquinio Teles <tarquinioteles@me.com>
2022-10-13 11:28:57 -0300

    extracted test setup to a helper method

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,22 +11,7 @@ import EssentialFeed
 final class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
-        
-        let exp = expectation(description: "Wait for load completion")
-        
-        var receivedResult: LoadFeedResult?
-        loader.load(completion: { result in
-            receivedResult = result
-
-            exp.fulfill()
-        })
-        
-        wait(for: [exp], timeout: 15.0)
-        
-        switch receivedResult {
+        switch getFeedResult() {
             case let .success(items)?:
                 XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
                 
@@ -48,6 +33,25 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     //MARK: - Helper
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load(completion: { result in
+            receivedResult = result
+
+            exp.fulfill()
+        })
+        
+        wait(for: [exp], timeout: 15.0)
+        
+        return receivedResult
+    }
     
     private func expectedItem(at index: Int) -> FeedItem {
         return FeedItem(
