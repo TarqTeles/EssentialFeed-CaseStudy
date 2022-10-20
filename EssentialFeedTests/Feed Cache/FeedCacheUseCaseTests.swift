7feb056cac68975539b8f4bc61be9a18376b6319
Tarquinio Teles <tarquinioteles@me.com>
2022-10-20 16:11:06 -0300

    Extracted out creation to  helper factory method to protect tests against breaking changes

--- EssentialFeedTests/Feed Cache/FeedCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/FeedCacheUseCaseTests.swift
@@ -32,15 +32,13 @@ class FeedStore {
 final class FeedCacheUseCaseTests: XCTestCase {
     
     func test_init_doesNotDeleteStoreUponCreation() {
-        let store = FeedStore()
-        _ = LocalFeedLoader(store: store)
-        
+        let (_, store) = makeSUT()
+
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
     
     func test_save_requestCacheDeletion() {
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
+        let (sut, store) = makeSUT()
         let items = [uniqueItem(), uniqueItem()]
         
         sut.save(items: items)
@@ -49,6 +47,13 @@ final class FeedCacheUseCaseTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+
+        return (sut, store)
+    }
     
     private func uniqueItem() -> FeedItem {
         return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
