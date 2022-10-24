62cb5836e673675e6cd108986e07bd9dfd40011a
Tarquinio Teles <tarquinioteles@me.com>
2022-10-24 11:27:20 -0300

    Load command delivers no images on empty cache

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -47,21 +47,27 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
     
-//    func test_load_deliverNoImagesOnEmptyCache() {
-//        let (sut, store) = makeSUT()
-//        let exp = expectation(description: "Wait for load completion")
-//
-//        var receivedImages: [FeedImage]?
-//        sut.load { result in
-//            receivedImages =
-//            exp.fulfill()
-//        }
-//
-//        store.completeRetrieval(with: retrievalError)
-//        wait(for: [exp], timeout: 1.0)
-//
-//        XCTAssertEqual(receivedImages, [])
-//    }
+    func test_load_deliverNoImagesOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        let exp = expectation(description: "Wait for load completion")
+
+        var receivedImages: [FeedImage]?
+        sut.load { result in
+            switch result {
+                case let .success(images):
+                    receivedImages = images
+
+                default:
+                    XCTFail("Expected success, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+
+        store.completeRetrievalWithEmptyCache()
+        wait(for: [exp], timeout: 1.0)
+
+        XCTAssertEqual(receivedImages, [])
+    }
     
     // MARK: - Helpers
     
