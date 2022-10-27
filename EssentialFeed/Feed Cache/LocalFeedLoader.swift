2fa9b8c6219afa20f92f27337f1aa8df56b41513
Tarquinio Teles <tarquinioteles@me.com>
2022-10-27 11:23:54 -0300

    Validate cache command does not delete cache when cache is already empty

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -55,8 +55,15 @@ public final class LocalFeedLoader {
     }
     
     public func validateCache() {
-        store.retrieve {_ in }
-        store.deleteCachedFeed {_ in }
+        store.retrieve { [unowned self] result in
+            switch result {
+                case .failure:
+                    self.store.deleteCachedFeed {_ in }
+                    
+                default:
+                    break
+            }
+        }
     }
 
     private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
