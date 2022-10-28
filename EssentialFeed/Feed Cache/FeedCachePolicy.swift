5e1b8e231170bd784cd75ef293ca516b54813306
Tarquinio Teles <tarquinioteles@me.com>
2022-10-28 11:51:19 -0300

    Moved FeedCachePolicy to its own file

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Tarquinio Teles on 28/10/22.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private static let calendar = Calendar(identifier: .gregorian)
+
+    private init() {}
+    
+    private static var maxCacheAgeInDays: Int { return 7 }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        
+        return date < maxCacheAge
+    }
+}
