cf4c94892c33b9c1222410197aa76eb5955ba105
Tarquinio Teles <tarquinioteles@me.com>
2022-11-04 10:42:37 -0300

    Extracted CoreDate managed models into their own files

---
+++ EssentialFeed/Feed Cache/ManagedFeedImage.swift
@@ -0,0 +1,33 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Tarquinio Teles on 04/11/22.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+    
+    static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        NSOrderedSet(array: feed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            
+            return managed
+        })
+    }
+    
+    var local: LocalFeedImage {
+        LocalFeedImage(id: self.id, description: self.imageDescription, location: self.location, url: self.url)
+    }
+}
