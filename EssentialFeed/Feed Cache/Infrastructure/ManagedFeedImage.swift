0a745d85228edf44dc1597716eb8775ad1686cf5
Tarquinio Teles <tarquinioteles@me.com>
2022-11-04 11:08:46 -0300

    Separated CoreData managed models data from helpers with extensions and corrected access control to internal

--- EssentialFeed/Feed Cache/Infrastructure/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/ManagedFeedImage.swift
@@ -9,13 +9,15 @@ import CoreData
 
 @objc(ManagedFeedImage)
 internal class ManagedFeedImage: NSManagedObject {
-    @NSManaged var id: UUID
-    @NSManaged var imageDescription: String?
-    @NSManaged var location: String?
-    @NSManaged var url: URL
-    @NSManaged var cache: ManagedCache
-    
-    static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+    @NSManaged internal var id: UUID
+    @NSManaged internal var imageDescription: String?
+    @NSManaged internal var location: String?
+    @NSManaged internal var url: URL
+    @NSManaged internal var cache: ManagedCache
+}
+
+extension ManagedFeedImage {
+    internal static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         NSOrderedSet(array: feed.map { local in
             let managed = ManagedFeedImage(context: context)
             managed.id = local.id
@@ -27,7 +29,7 @@ internal class ManagedFeedImage: NSManagedObject {
         })
     }
     
-    var local: LocalFeedImage {
+    internal var local: LocalFeedImage {
         LocalFeedImage(id: self.id, description: self.imageDescription, location: self.location, url: self.url)
     }
 }
