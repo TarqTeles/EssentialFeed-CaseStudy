2d9b3e40ea7fe4335abf8b908cf6fd231714b91e
Tarquinio Teles <tarquinioteles@me.com>
2022-11-21 08:32:02 -0300

    Replaced custom LoadFeedResult enum with the standard Swift Result

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-internal final class FeedItemsMapper {
+final class FeedItemsMapper {
     private struct root: Decodable {
         let items: [RemoteFeedItem]
     }
 
     private static var OK_200: Int { return 200 }
     
-    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(root.self, from: data) else {
             throw RemoteFeedLoader.Error.invalidData
         }
--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -8,24 +8,24 @@
 import CoreData
 
 @objc(ManagedCache)
-internal class ManagedCache: NSManagedObject {
-    @NSManaged internal var timestamp: Date
-    @NSManaged internal var feed: NSOrderedSet
+class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
 }
 
 extension ManagedCache {
-    internal static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
         request.returnsObjectsAsFaults = false
         return try context.fetch(request).first
     }
     
-    internal static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
         try ManagedCache.find(in: context).map(context.delete)
         return ManagedCache(context: context)
     }
     
-    internal var localFeed: [LocalFeedImage] {
+    var localFeed: [LocalFeedImage] {
         feed
             .compactMap { ($0 as? ManagedFeedImage)?.local }
     }
