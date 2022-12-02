314efbde0d2b9bbd07a7d6387f524b32f121fbea
Tarquinio Teles <tarquinioteles@me.com>
2022-12-02 16:46:03 -0300

    Rendered loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Tarquinio Teles on 02/12/22.
+//
+
+import UIKit
+
+public class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
