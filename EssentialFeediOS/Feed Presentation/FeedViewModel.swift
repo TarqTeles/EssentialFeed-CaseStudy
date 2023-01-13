272924ebc740c19f59166f16e88f9b722eb81344
Tarquinio Teles <tarquinioteles@me.com>
2023-01-13 15:53:56 -0300

    Moved FeedViewModel and FeedLoadingViewModel to their own files

---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Tarquinio Teles on 13/01/23.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
