b64fd21982eb54121582e27a12862f0315bb118a
Tarquinio Teles <tarquinioteles@me.com>
2023-01-20 15:36:51 -0300

    Localized feed view title string

--- EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -5,6 +5,7 @@
 //  Created by Tarquinio Teles on 13/01/23.
 //
 
+import Foundation
 import EssentialFeed
 
 protocol FeedLoadingView {
@@ -24,7 +25,12 @@ final class FeedPresenter {
         self.feedView = feedView
     }
     
-    static var title: String { "My Feed" }
+    static var title: String {
+        NSLocalizedString("FEED_VIEW_TITLE",
+                          tableName: "Feed",
+                          bundle: Bundle(for: FeedPresenter.self),
+                          comment: "Title for the Feed View")
+    }
     
     func didStartLoadingFeed() {
         loadingView.display(FeedLoadingViewModel(isLoading: true))
--- EssentialFeediOSTests/Feed UI - Controllers/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/Feed UI - Controllers/FeedViewControllerTests.swift
@@ -16,7 +16,12 @@ class FeedViewControllerTests: XCTestCase {
         
         sut.loadViewIfNeeded()
         
-        XCTAssertEqual(sut.title, "My Feed")
+        let bundle = Bundle(for: FeedViewController.self)
+        let localizedKey = "FEED_VIEW_TITLE"
+        let localizedTitle = bundle.localizedString(forKey: localizedKey, value: nil, table: "Feed")
+        
+        XCTAssertNotEqual(localizedTitle, localizedKey, "Missing localized string for key: \(localizedKey)")
+        XCTAssertEqual(sut.title, localizedTitle)
     }
     
     func test_loadFeedActions_requestFeedFromLoader() {
