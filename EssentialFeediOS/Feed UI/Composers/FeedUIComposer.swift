d32f3f0c26957557c2cce4ba2712a0478c0ffeba
Tarquinio Teles <tarquinioteles@me.com>
2023-01-23 16:50:15 -0300

    Moved MainThreadDispatchDecorator to a separate file

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -36,37 +36,6 @@ private extension FeedViewController {
     }
 }
 
-private final class MainThreadDispatchDecorator<T> {
-    private let decoratee: T
-    
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-        completion()
-    }
-}
-
-extension MainThreadDispatchDecorator: FeedLoader where T == FeedLoader {
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
-
-extension MainThreadDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        decoratee.loadImageData(from: url) { [weak self] result in
-            self?.dispatch {completion(result) }
-        }
-    }
-}
-
 private final class WeakRefVirtualProxy<T: AnyObject> {
     private weak var object: T?
     
---
+++ EssentialFeediOS/Feed UI/Composers/MainThreadDispatchDecoratorMainThreadDispatchDecorator.swift
@@ -0,0 +1,39 @@
+//
+//  MainThreadDispatchDecoratorMainThreadDispatchDecorator.swift
+//  EssentialFeediOS
+//
+//  Created by Tarquinio Teles on 23/01/23.
+//
+
+import EssentialFeed
+
+final class MainThreadDispatchDecorator<T> {
+    private let decoratee: T
+    
+    init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        completion()
+    }
+}
+
+extension MainThreadDispatchDecorator: FeedLoader where T == FeedLoader {
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
+
+extension MainThreadDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        decoratee.loadImageData(from: url) { [weak self] result in
+            self?.dispatch {completion(result) }
+        }
+    }
+}
