f01bffe4f26fea10764dfe6959111511d0bd6405
Tarquinio Teles <tarquinioteles@me.com>
2023-03-06 12:34:24 -0300

    RemoteFeedImageDataLoader delivers error on client error

--- EssentialFeedTests/Feed API/EssentialFeed:EssentialFeedTests:Feed API:RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/EssentialFeed:EssentialFeedTests:Feed API:RemoteFeedImageDataLoaderTests.swift
@@ -15,8 +15,14 @@ final class RemoteFeedImageDataLoader {
         self.client = client
     }
     
-    func loadImageData(from url: URL) {
-        client.get(from: url, completion: {_ in })
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+                case let .failure(error): completion(.failure(error))
+                    
+                default: break
+            }
+        }
     }
 }
 
@@ -32,7 +38,7 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.loadImageData(from: url)
+        sut.loadImageData(from: url) {_ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
@@ -41,13 +47,20 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.loadImageData(from: url)
-        sut.loadImageData(from: url)
+        sut.loadImageData(from: url) {_ in }
+            sut.loadImageData(from: url) {_ in }
 
         XCTAssertEqual(client.requestedURLs, [url, url])
-
     }
     
+    func test_loadImageDataFromURL_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        let clientError = NSError(domain: "a client error", code: 0)
+        
+        expect(sut, toCompleteWith: .failure(clientError), when: {
+            client.completeWithError(clientError)
+        })
+    }
     
     // MARK: - Helpers
     
@@ -59,11 +72,42 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         return (sut, client)
     }
     
-    private final class HTTPClientSpy: HTTPClient {
-        private(set) public var requestedURLs = [URL]()
+    private func expect(_ sut: RemoteFeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let url = URL(string: "https://a-given-url.com")!
+        let exp = expectation(description: "waiting for load cpmpletion")
         
-        func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
-            requestedURLs.append(url)
+        sut.loadImageData(from: url) { receivedResult in
+            switch (receivedResult, expectedResult) {
+                case let (.success(receivedData), .success(expectedData)):
+                    XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+                    
+                case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                    XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                    
+                default:
+                    XCTFail("Expected \(expectedResult), received \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        private(set) public var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+        
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
+        
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            messages.append((url, completion))
+        }
+                
+        func completeWithError(_ error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
         }
     }
 }
