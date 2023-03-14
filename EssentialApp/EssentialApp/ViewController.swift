dc1be7ce7d8180e127ed32d0e89d39b702b8246c
Tarquinio Teles <tarquinioteles@me.com>
2023-03-14 15:49:26 -0300

    Added empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Tarquinio Teles on 14/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
