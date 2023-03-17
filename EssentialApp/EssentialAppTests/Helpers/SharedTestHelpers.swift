daee425503949fd237c33b5a87112aca5b21e2b2
Tarquinio Teles <tarquinioteles@me.com>
2023-03-17 10:06:19 -0300

    Extracted uniqueFeed factory method into a shared scope to remove duplication

--- EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -5,10 +5,14 @@
 //  Created by Tarquinio Teles on 16/03/23.
 //
 
-import Foundation
+import EssentialFeed
 
 func anyNSError() -> NSError{ NSError(domain: "any error", code: 1) }
 
 func anyURL() -> URL {
     URL(string: "https://any-url.com")!
+}
+
+func uniqueFeed() -> [FeedImage] {
+    return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
 }
