83a4086a9db4951a43a5d5434bb7d4e1297d7c39
Tarquinio Teles <tarquinioteles@me.com>
2023-04-25 15:12:32 -0300

    Added ImageComment data model

---
+++ EssentialFeed/FeedFeature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssentialFeed
+//
+//  Created by Tarquinio Teles on 25/04/23.
+//
+
+import Foundation
+
+public struct ImageComment: Hashable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
