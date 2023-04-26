a3caa2850be8a2759ecdc028846fdda5f4b536e9
Tarquinio Teles <tarquinioteles@me.com>
2023-04-26 11:38:40 -0300

    Added iPhone 13 profile for snapstots

--- EssentialApp/EssentialAppTests/Helpers/UIViewController+Snapshot.swift
+++ EssentialApp/EssentialAppTests/Helpers/UIViewController+Snapshot.swift
@@ -36,6 +36,25 @@ struct SnapshotConfiguration {
                 .init(userInterfaceStyle: style)
             ]))
     }
+
+    static func iPhone13(style: UIUserInterfaceStyle, contentSize: UIContentSizeCategory = .medium) -> SnapshotConfiguration {
+        return SnapshotConfiguration(
+            size: CGSize(width: 390, height: 844),
+            safeAreaInsets: UIEdgeInsets(top: 47, left: 0, bottom: 34, right: 0),
+            layoutMargins: UIEdgeInsets(top: 55, left: 8, bottom: 42, right: 8),
+            traitCollection: UITraitCollection(traitsFrom: [
+                .init(forceTouchCapability: .unavailable),
+                .init(layoutDirection: .leftToRight),
+                .init(preferredContentSizeCategory: contentSize),
+                .init(userInterfaceIdiom: .phone),
+                .init(horizontalSizeClass: .compact),
+                .init(verticalSizeClass: .regular),
+                .init(displayScale: 3),
+                .init(accessibilityContrast: .normal),
+                .init(displayGamut: .P3),
+                .init(userInterfaceStyle: style)
+            ]))
+    }
 }
 
 private final class SnapshotWindow: UIWindow {
