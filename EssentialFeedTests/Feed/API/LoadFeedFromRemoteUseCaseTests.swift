1c58eeb529c7b9dae69bd44a69c58e1d0ca8fc0c
Tarquinio Teles <tarquinioteles@me.com>
2023-04-26 10:09:49 -0300

    Replaced RemoteFeedLoader with generic RemoteLoader to remove duplication

--- EssentialFeedTests/Feed/API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed/API/LoadFeedFromRemoteUseCaseTests.swift
@@ -10,40 +10,6 @@ import EssentialFeed
 
 final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 
-    func test_init_doesNotRequestDataFromURL() {
-        let (_, client) = makeSUT()
-                
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-    
-    func test_load_requestsDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-        sut.load { _ in }
-
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-
-        expect(sut, toCompleteWith: failure(.connectivity), when: {
-            let clientError = NSError(domain: "Test", code: 0)
-            client.complete(with: clientError)
-        })
-    }
-    
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
 
@@ -89,20 +55,6 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
             let json = makeItemsJSON([item1.json, item2.json])
             client.complete(withStatusCode: 200, data: json)
         })
-    }
-    
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "https://any-url.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
-        
-        var capturedResult = [RemoteFeedLoader.Result]()
-        sut?.load { capturedResult.append($0) }
-
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
-        
-        XCTAssertTrue(capturedResult.isEmpty)
     }
     
     //MARK: -> Helpers
