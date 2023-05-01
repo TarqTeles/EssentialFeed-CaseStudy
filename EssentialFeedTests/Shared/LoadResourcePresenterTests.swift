a9569e1c31d2766756ece2e4bf90a2bb06999c52
Tarquinio Teles <tarquinioteles@me.com>
2023-05-01 14:10:11 -0300

    Renamed method

--- EssentialFeedTests/Shared/LoadResourcePresenterTests.swift
+++ EssentialFeedTests/Shared/LoadResourcePresenterTests.swift
@@ -16,10 +16,10 @@ final class LoadResourcePresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages on init")
     }
     
-    func test_didStartLoadingFeed_displaysNoErrorMessageAndStartsLoading() {
+    func test_didStartLoading_displaysNoErrorMessageAndStartsLoading() {
         let (sut, view) = makeSUT()
         
-        sut.didStartLoadingFeed()
+        sut.didStartLoading()
         
         XCTAssertEqual(view.messages, [.display(errorMessage: .none),
                                        .display(isLoading: true)])
