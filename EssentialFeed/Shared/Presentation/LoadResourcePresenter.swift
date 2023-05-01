1921f7d4e29cc33a98670937b8bb4f30c4743eb5
Tarquinio Teles <tarquinioteles@me.com>
2023-05-01 13:41:42 -0300

    Duplicated FeedPresenter as LoadResourcePresenter

---
+++ EssentialFeed/Shared/Presentation/LoadResourcePresenter.swift
@@ -0,0 +1,39 @@
+//
+//  LoadResourcePresenter.swift
+//  EssentialFeed
+//
+//  Created by Tarquinio Teles on 01/05/23.
+//
+
+import Foundation
+
+public final class LoadResourcePresenter {
+    private let loadingView: FeedLoadingView
+    private let feedView: FeedView
+    private let errorView: FeedErrorView
+
+    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+        self.feedView = feedView
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
+    
+    public static var title: String {
+        Localized.Feed.title
+    }
+    
+    public func didStartLoadingFeed() {
+        errorView.display(.noError)
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+    
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+    
+    public func didFinishLoadingFeed(with error: Error) {
+        errorView.display(.error(message: Localized.Feed.loadError))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+}
