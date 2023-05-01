7a7b80d385426c330143fd606646ba0c86a200f4
Tarquinio Teles <tarquinioteles@me.com>
2023-05-01 19:34:52 -0300

    Added FeedImagePresenter .map

--- EssentialFeed/Feed Presentation/FeedImagePresenter.swift
+++ EssentialFeed/Feed Presentation/FeedImagePresenter.swift
@@ -49,5 +49,14 @@ public final class FeedImagePresenter<View: FeedImageView, Image> where View.Ima
             isLoading: false,
             shouldRetry: true))
     }
+    
+    public static func map(_ image: FeedImage) -> FeedImageViewModel<Image> {
+        FeedImageViewModel(
+            description: image.description,
+            location: image.location,
+            image: nil,
+            isLoading: false,
+            shouldRetry: false)
+    }
 }
 
