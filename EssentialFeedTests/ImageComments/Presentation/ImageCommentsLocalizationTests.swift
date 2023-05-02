83b6c00f75197a578cc587a4e3a72220777887d7
Tarquinio Teles <tarquinioteles@me.com>
2023-05-02 09:46:17 -0300

    Added localization tests for ImageComments

---
+++ EssentialFeedTests/ImageComments/Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCommentsLocalizationTests.swift
+//  EssentialFeedTests
+//
+//  Created by Tarquinio Teles on 02/05/23.
+//
+
+import XCTest
+import EssentialFeed
+
+final class ImageCommentsLocalizationTests: XCTestCase {
+
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+
+}
