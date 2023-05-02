c0cce1e11ddf296e3218698c25956ac550548ebc
Tarquinio Teles <tarquinioteles@me.com>
2023-05-02 15:29:04 -0300

    Changed references of Feed to List

--- EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -11,22 +11,22 @@ import EssentialFeediOS
 
 final class ListSnapshotTests: XCTestCase {
 
-    func test_emptyFeed() {
+    func test_emptyList() {
         let sut = makeSUT()
         
         sut.display(emptyFeed())
         
-        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "EMPTY_FEED_light")
-        assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "EMPTY_FEED_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "EMPTY_LIST_light")
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "EMPTY_LIST_dark")
     }
     
-    func test_feedWithErrorMessage() {
+    func test_listWithErrorMessage() {
         let sut = makeSUT()
         
         sut.display(ResourceErrorViewModel(message: "This is a\nmulti-line\nerror message"))
         
-        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
-        assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
     }
     
     // MARK: - Helpers
