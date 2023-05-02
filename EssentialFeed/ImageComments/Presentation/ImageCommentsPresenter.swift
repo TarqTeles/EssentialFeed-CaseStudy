0d2ff10ca8a278af24b19f1ad0d459fc6f6ec7a0
Tarquinio Teles <tarquinioteles@me.com>
2023-05-02 10:41:12 -0300

    Mapped ImageComments into ImageCommentViewModels

--- EssentialFeed/ImageComments/Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/ImageComments/Presentation/ImageCommentsPresenter.swift
@@ -7,8 +7,37 @@
 
 import Foundation
 
+public struct ImageCommentsViewModel {
+    public let comments: [ImageCommentViewModel]
+}
+
+public struct ImageCommentViewModel: Equatable {
+    public let message: String
+    public let date: String
+    public let username: String
+    
+    public init(message: String, date: String, username: String) {
+        self.message = message
+        self.date = date
+        self.username = username
+    }
+}
+
 public final class ImageCommentsPresenter {
     public static var title: String {
         Localized.ImageComments.title
     }
+    
+    public static func map(_ comments: [ImageComment]) -> ImageCommentsViewModel {
+        let formatter = RelativeDateTimeFormatter()
+        
+        return ImageCommentsViewModel(comments: comments.map { comment in
+            ImageCommentViewModel(
+                message: comment.message,
+                date: formatter.localizedString(for: comment.createdAt, relativeTo: Date()),
+                username: comment.username
+            )
+        })
+    }
 }
+
--- EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -22,3 +22,19 @@ func makeItemsJSON(_ items: [[String:Any]]) -> Data {
     let json = [ "items" : items ]
     return try! JSONSerialization.data(withJSONObject: json)
 }
+
+extension Date {
+    func adding(minutes: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .minute, value: minutes, to: self)!
+    }
+    
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+}
+
+extension Date {
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
+    }
+}
