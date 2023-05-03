996fceaee9025d468d221b9eb08d06f09b861ca3
Tarquinio Teles <tarquinioteles@me.com>
2023-05-02 22:03:29 -0300

    Now running list snapshot tests without a storyboard

--- EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -32,9 +32,8 @@ final class ListSnapshotTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT() -> ListViewController {
-        let bundle = Bundle(for: ListViewController.self)
-        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        let controller = ListViewController()
+        controller.tableView.separatorStyle = .none
         controller.loadViewIfNeeded()
         controller.tableView.showsVerticalScrollIndicator = false
         controller.tableView.showsHorizontalScrollIndicator = false
