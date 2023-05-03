7f4029deb07e4583048f38a6db7b675a1b3896f0
Tarquinio Teles <tarquinioteles@me.com>
2023-05-03 15:40:56 -0300

    Moved shared helpers to shared scope

--- EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -5,7 +5,7 @@
 //  Created by Tarquinio Teles on 16/03/23.
 //
 
-import EssentialFeed
+@testable import EssentialFeed
 
 func anyNSError() -> NSError{ NSError(domain: "any error", code: 1) }
 
@@ -15,4 +15,16 @@ func anyURL() -> URL {
 
 func uniqueFeed() -> [FeedImage] {
     return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
+}
+
+var loadError: String {
+    Localized.Shared.loadError
+}
+
+var feedTitle: String {
+    Localized.Feed.title
+}
+
+var commentsTitle: String {
+    Localized.ImageComments.title
 }
