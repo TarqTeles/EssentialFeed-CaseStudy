7f7aac62cc77d18b0adfc3041daffc9ab3c6dbfd
Tarquinio Teles <tarquinioteles@me.com>
2023-05-12 11:36:17 -0300

    Fixed conflicting constraints warning (LoadMoreCell was resizing out of view due to predictive loading)

--- EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
+++ EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
@@ -36,7 +36,8 @@ public final class LoadMoreCell: UITableViewCell {
             label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
             contentView.trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: 8),
             label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            contentView.bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: 8)
+            contentView.bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: 8),
+            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40)
         ])
         return label
     }()
