b4870e27697d4ae499a6c591cc082fdd1edad649
Tarquinio Teles <tarquinioteles@me.com>
2023-05-15 18:03:12 -0300

    Renamed method to clarify intent

--- EssentialFeed/Feed/Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/Feed/Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -35,7 +35,7 @@ public final class CoreDataFeedStore {
         }
     }
     
-    func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+    func performAsync(_ action: @escaping (NSManagedObjectContext) -> Void) {
         let context = self.context
         context.perform { action(context) }
     }
